3f6fb28f6a92d873b3b60103fafe8107eadfad0c
Steven Perfors <stevecanbuild@gmail.com>
2020-02-27 20:30:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-  if array.length 
+  if array.length < 2 
+    array.join()
+  else if array.length < 3
+    array.join(" and ")
+  else 
+    array.join(" , ", " and ")
 end
